e93936884c6c81a244b9a134ab84a964020d6e9b
saad khan <s2ksaadkhan@gmail.com>
2023-02-08 10:53:10 -0500

    added update accounts

--- service/routes.py
+++ service/routes.py
@@ -93,6 +93,25 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
 
+######################################################################
+# UPDATE AN EXISTING ACCOUNT
+######################################################################
+
+# ... place you code here to UPDATE an account ...
+
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_accounts(account_id):
+    """
+    Update an Account
+    This endpoint will update an Account based on the posted data
+    """
+    app.logger.info("Request to update an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+    account.deserialize(request.get_json())
+    account.update()
+    return account.serialize(), status.HTTP_200_OK
     
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
