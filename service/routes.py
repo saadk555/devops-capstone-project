6d8999d9e5199e4e656fefdc4d801e2243eab636
saad khan <s2ksaadkhan@gmail.com>
2023-02-08 10:49:54 -0500

    added read accounts

--- service/routes.py
+++ service/routes.py
@@ -74,7 +74,26 @@ def list_accounts():
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
+   
+######################################################################
+# READ AN ACCOUNT
+######################################################################
 
+# ... place you code here to READ an account ...
+
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def get_accounts(account_id):
+    """
+    Reads an Account
+    This endpoint will read an Account based the account_id that is requested
+    """
+    app.logger.info("Request to read an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+    return account.serialize(), status.HTTP_200_OK
+
+    
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
